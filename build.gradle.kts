8dc3ad41147f1e6af3f2bd21d8f4789a76904484
Zen Huifer <huifer97@163.com>
2023-01-10 09:48:24 +0800

    fix: 修正可用版本

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.springframework.boot.gradle.tasks.bundling.BootBuildImage
 
 plugins {
     java
-    id("org.springframework.boot") version "2.7.7"
+    id("org.springframework.boot") version "2.6.7"
     id("io.spring.dependency-management") version "1.0.15.RELEASE"
 }
 
@@ -26,17 +26,15 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.mybatis.spring.boot:mybatis-spring-boot-starter:2.3.0")
     compileOnly("org.projectlombok:lombok")
-    developmentOnly("org.springframework.boot:spring-boot-devtools")
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
     annotationProcessor("org.projectlombok:lombok")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     implementation("com.baomidou:mybatis-plus-boot-starter:3.5.2")
     implementation("mysql:mysql-connector-java")
-    implementation("org.apache.shardingsphere:shardingsphere-jdbc-core-spring-boot-starter:5.2.1")
+    implementation("org.apache.shardingsphere:shardingsphere-jdbc-core-spring-boot-starter:5.2.0")
 
     implementation("cn.hutool:hutool-all:5.8.11")
     implementation("com.baomidou:mybatis-plus-boot-starter:3.5.3.1")
-
     implementation("org.mybatis:mybatis-typehandlers-jsr310:1.0.2")
 
 }
--- src/main/java/com/example/demo/sharding/CustomStandardShardingAlgorithm.java
+++ src/main/java/com/example/demo/sharding/CustomStandardShardingAlgorithm.java
@@ -1,22 +1,30 @@
 package com.example.demo.sharding;
 
+import java.util.Collection;
+import java.util.Date;
 import java.util.Properties;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.time.DateFormatUtils;
+import org.apache.commons.lang.time.DateFormatUtils;
 import org.apache.shardingsphere.sharding.api.sharding.standard.PreciseShardingValue;
 import org.apache.shardingsphere.sharding.api.sharding.standard.RangeShardingValue;
 import org.apache.shardingsphere.sharding.api.sharding.standard.StandardShardingAlgorithm;
 
-import java.util.Collection;
-import java.util.Date;
-import java.util.Objects;
-
+/**
+ * 定义历史表
+ *
+ * @author lance
+ * @date 2022/3/5 21:28
+ */
 @Slf4j
 public final class CustomStandardShardingAlgorithm implements StandardShardingAlgorithm<Date> {
-  @Override
-  public String doSharding(Collection<String> collection, PreciseShardingValue<Date> shardingValue) {
 
-    return null;
+  @Override
+  public String doSharding(Collection<String> collection,
+      PreciseShardingValue<Date> shardingValue) {
+    Date date = shardingValue.getValue();
+    String suffix = DateFormatUtils.format(date, "yyyyMM");
+
+    return shardingValue.getLogicTableName() + "_" + suffix;
   }
 
   @Override
@@ -24,18 +32,16 @@ public final class CustomStandardShardingAlgorithm implements StandardShardingAl
     return collection;
   }
 
-  public void init() {
 
+
+  @Override
+  public Properties getProps() {
+    return null;
   }
 
   @Override
   public void init(Properties props) {
 
-  }
-
-  @Override
-  public Properties getProps() {
-    return null;
   }
 
   @Override
